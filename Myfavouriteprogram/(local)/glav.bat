18b343653e1b0c74fff1a6e12e8bb13ae49c7ff3
Vladanak <vladmyr89@gmail.com>
2018-11-26 23:18:38 +0300

    Update-1

--- Myfavouriteprogram/(local)/glav.bat
+++
@@ -1,8 +0,0 @@
-RD /Q /S "%USERPROFILE%"\AppData\Local\Opera Software\Opera Stable\Cache
-RD /Q /S "%USERPROFILE%"\AppData\Local\"Opera Software"\"Opera Stable"\"Media Cache"
-RD /Q /S "%USERPROFILE%"\"Local Settings"\"Application Data"\Google\Chrome\"User Data"\Default\Cache
-RD /Q /S "%USERPROFILE%"\AppData\local\Mozilla\Firefox\Profile
-
-
-
-
--- README.md
+++
@@ -1,3 +0,0 @@
-# Python
-Myfavouriteprogram contains program which is not finished!But I promise it'll be cool).
-Training contains program which was writed on framework Flask.
